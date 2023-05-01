5fb2a342c3181f141e43b6ff4e7eedb940d1581b
448623761 <448623761@qq.com>
2023-05-01 22:03:15 +0800

    111

--- config.inc.php
+++ config.inc.php
@@ -1,4 +1,6 @@
 <?php
+
+define('__TYPECHO_SECURE__',true);
 // site root path
 define('__TYPECHO_ROOT_DIR__', dirname(__FILE__));
 
@@ -11,15 +13,19 @@ define('__TYPECHO_THEME_DIR__', '/usr/themes');
 // admin directory (relative path)
 define('__TYPECHO_ADMIN_DIR__', '/admin/');
 
+@set_include_path(get_include_path() . PATH_SEPARATOR .
+__TYPECHO_ROOT_DIR__ . '/var' . PATH_SEPARATOR .
+__TYPECHO_ROOT_DIR__ . __TYPECHO_PLUGIN_DIR__);
+
 // register autoload
-require_once __TYPECHO_ROOT_DIR__ . '/var/Typecho/Common.php';
+require_once 'Typecho/Common.php';
 
 // init
-\Typecho\Common::init();
+Typecho_Common::init();
 
 // config db
-$db = new \Typecho\Db('Pdo_SQLite', 'typecho_');
+$db = new Typecho_Db('Pdo_SQLite', 'typecho_');
 $db->addServer(array (
   'file' => __TYPECHO_ROOT_DIR__.'/usr/644fb1c4692f6.db',
-), \Typecho\Db::READ | \Typecho\Db::WRITE);
-\Typecho\Db::set($db);
+), Typecho_Db::READ | Typecho_Db::WRITE);
+Typecho_Db::set($db);
